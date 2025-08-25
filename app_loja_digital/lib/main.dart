36a121c6fecc24c140eaccd9aa779a51f48d4188
jvbr7 <joaovitorbarbozaricardo7@gmail.com>
2025-08-25 00:43:31 -0300

    iniciei o firebase

---
+++ app_loja_digital/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); 
+
+  runApp(const MyApp());
+
+  FirebaseFirestore.instance.collection('teste').add({'testee': 'TESTE'});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: Container(),
+    );
+  }
+}
